9ddcb25dedb26eec67912c7143ec86901d882e0e
eneskemalergin <eneskemalergin@gmail.com>
2015-02-11 18:25:10 -0600

    directory_exists upgraded to  V.0.2.1

--- directory_exists.py
+++ directory_exists.py
@@ -1,13 +1,26 @@
 ## Function for checking and creating folders.
-## Version 0.1
-  ## - User must put the path when calling the function as a parameter.
-  ## - 
-def directory_exist(directory):
+## Version 0.2.1
+  ## - Parameters:
+  ##    * base_url
+  ##    * topic_name
+  ##    * program_name
+  ## It creates a folders with topic_name, and program_name
+def directory_exist_V2(base_url, program_name, topic_name):
     import os
-    #base_path = input("Please put your file path using the sample form \n C:/user/pc-41/desktop :")
-    #directory = base_path + program_name + topic_name      
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    # Checks the first path of a directory.
+    directory_one  = base_url + '/' + program_name  
+    if not os.path.exists(directory_one):
+        os.makedirs(directory_one)
         print("New directory created...")
     else:
         print("You already have specified directory...")
+    
+    # Checkst the second level path of a directory.
+    directory_two = directory_one + '/' + topic_name 
+    if not os.path.exists(directory_two):
+        os.makedirs(directory_two)
+        print("New directory created...")
+        return(directory_two)
+    else:
+        print("You already have specidied directory...")                 
+        return(directory_two) 
